3eee15e0ffe83eacddcff9f37e31347f3e08eddb
Zach Stone <zach@zerovolts.com>
2020-12-23 00:09:57 -0500

    Add Rect iterator

--- src/patterns.rs
+++ src/patterns.rs
@@ -43,6 +43,61 @@ pub fn diag_neighborhood<'a, C: Into<Coord>>(coord: C) -> impl Iterator<Item = C
         .map(move |&offset| coord + offset.into())
 }
 
+pub fn rect(from_corner: Coord, to_corner: Coord) -> impl Iterator<Item = Coord> {
+    let bounds = RectBounds {
+        top: from_corner.y.max(to_corner.y),
+        bottom: from_corner.y.min(to_corner.y),
+        left: from_corner.x.min(to_corner.x),
+        right: from_corner.x.max(to_corner.x),
+    };
+    let next_coord = Coord::new(bounds.left, bounds.bottom);
+
+    RectIter {
+        bounds,
+        next_coord,
+        is_finished: false,
+    }
+}
+
+struct RectBounds {
+    top: i32,
+    bottom: i32,
+    left: i32,
+    right: i32,
+}
+
+/// Iterates row by row from the bottom-left corner to the top-right corner.
+struct RectIter {
+    bounds: RectBounds,
+    next_coord: Coord,
+    is_finished: bool,
+}
+
+impl Iterator for RectIter {
+    type Item = Coord;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.is_finished {
+            return None;
+        }
+
+        // We return the coordinate computed on the previous iteration.
+        let next_coord = self.next_coord;
+
+        if self.next_coord.x < self.bounds.right {
+            self.next_coord.x += 1;
+        } else if self.next_coord.y < self.bounds.top {
+            self.next_coord.x = self.bounds.left;
+            self.next_coord.y += 1;
+        } else {
+            // We've passed the last element.
+            self.is_finished = true;
+        }
+
+        Some(next_coord)
+    }
+}
+
 /// Traces Bresenham's line algorithm between `from` and `to`.
 pub fn line(from: Coord, to: Coord) -> impl Iterator<Item = Coord> {
     let delta = to - from;
@@ -101,7 +156,7 @@ impl Iterator for LineIter {
             return Some(self.end_coord);
         }
 
-        // We return the coordinate computed on the previous iteration
+        // We return the coordinate computed on the previous iteration.
         let return_coord = self.next_coord;
 
         self.next_coord += self.major_step;
@@ -211,5 +266,24 @@ mod tests {
         assert!(layers.interior.len() == 1);
         assert!(layers.internal_border.len() == 8);
         assert!(layers.external_border.len() == 16);
+    }
+
+    #[test]
+    fn single_coord_rect_iter() {
+        let coord_count = rect(Coord::new(0, 0), Coord::new(0, 0)).count();
+        assert_eq!(coord_count, 1);
+    }
+
+    #[test]
+    fn multi_coord_rect_iter() {
+        let coord_count = rect(Coord::new(0, 0), Coord::new(3, 3)).count();
+        assert_eq!(coord_count, 16);
+    }
+
+    #[test]
+    fn reversed_coord_rect_iter() {
+        let coords = rect(Coord::new(2, 2), Coord::new(-2, -2)).collect::<Vec<_>>();
+        assert_eq!(coords.first(), Some(&Coord::new(-2, -2)));
+        assert_eq!(coords.last(), Some(&Coord::new(2, 2)));
     }
 }
