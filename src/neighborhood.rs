0e78b528830f667b61925568ce5ef7ae713838c1
Zach Stone <zach@zerovolts.com>
2021-04-10 16:18:36 -0400

    Add consuming iterator methods to Neighborhood

--- src/neighborhood.rs
+++ src/neighborhood.rs
@@ -1,5 +1,20 @@
 use crate::coord::Coord;
 
+const NEIGHBOR_OFFSETS: [(i32, i32); 8] = [
+    (0, 1),
+    (1, 1),
+    (1, 0),
+    (1, -1),
+    (0, -1),
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+];
+
+const ORTHO_NEIGHBOR_OFFSETS: [(i32, i32); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
+
+const DIAG_NEIGHBOR_OFFSETS: [(i32, i32); 4] = [(1, 1), (1, -1), (-1, -1), (-1, 1)];
+
 pub struct Neighborhood(Coord);
 
 impl Neighborhood {
@@ -9,30 +24,42 @@ impl Neighborhood {
 
     /// Returns the orthogonal and diagonal (Moore) neighborhood of `coord`.
     pub fn iter(&self) -> impl Iterator<Item = Coord> + '_ {
-        [
-            (0, 1),
-            (1, 1),
-            (1, 0),
-            (1, -1),
-            (0, -1),
-            (-1, -1),
-            (-1, 0),
-            (-1, 1),
-        ]
-        .iter()
-        .map(move |&offset| self.0 + offset.into())
+        NEIGHBOR_OFFSETS
+            .iter()
+            .map(move |&offset| self.0 + offset.into())
+    }
+
+    /// Returns the orthogonal and diagonal (Moore) neighborhood of `coord`.
+    pub fn into_iter(self) -> impl Iterator<Item = Coord> {
+        NEIGHBOR_OFFSETS
+            .iter()
+            .map(move |&offset| self.0 + offset.into())
     }
 
     /// Returns the orthogonal (Von Neumann) neighborhood of `coord`.
     pub fn iter_ortho(&self) -> impl Iterator<Item = Coord> + '_ {
-        [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        ORTHO_NEIGHBOR_OFFSETS
+            .iter()
+            .map(move |&offset| self.0 + offset.into())
+    }
+
+    /// Returns the orthogonal (Von Neumann) neighborhood of `coord`.
+    pub fn into_iter_ortho(self) -> impl Iterator<Item = Coord> {
+        ORTHO_NEIGHBOR_OFFSETS
             .iter()
             .map(move |&offset| self.0 + offset.into())
     }
 
     /// Returns the diagonal neighborhood of `coord` (for completeness).
     pub fn iter_diag(&self) -> impl Iterator<Item = Coord> + '_ {
-        [(1, 1), (1, -1), (-1, -1), (-1, 1)]
+        DIAG_NEIGHBOR_OFFSETS
+            .iter()
+            .map(move |&offset| self.0 + offset.into())
+    }
+
+    /// Returns the diagonal neighborhood of `coord` (for completeness).
+    pub fn into_iter_diag(self) -> impl Iterator<Item = Coord> {
+        DIAG_NEIGHBOR_OFFSETS
             .iter()
             .map(move |&offset| self.0 + offset.into())
     }
